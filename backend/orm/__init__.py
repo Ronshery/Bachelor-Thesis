f15c190aa296bc00429e09cf1f7b8f0e29fc6de5
mcd01 <57414190+mcd01@users.noreply.github.com>
2022-06-20 19:28:33 +0200

    fix persistence

--- backend/orm/__init__.py
+++ backend/orm/__init__.py
@@ -1,9 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import registry
 
 import orm.models as orm_models
 
-engine = create_engine("sqlite+pysqlite:///test.sqlite")
+path_to_db: str = "sqlite+pysqlite:///test.sqlite"
+# simple check: we are running in kubernetes, i.e., this env variable is defined
+if len(os.environ.get("PROMETHEUS_ENDPOINT", "")):
+    path_to_db = "sqlite+pysqlite:////tmp/test.sqlite"
+
+engine = create_engine(path_to_db)
 
 
 def create_tables():
